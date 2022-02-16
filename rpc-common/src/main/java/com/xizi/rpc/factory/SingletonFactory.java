0bf74502c7c9187e4540370ceaaf056ebf58f429
yinpeng8 <yinpeng8@hikvision.com.cn>
2022-02-16 11:04:22 +0800

    [fix] 双重检测机制DCL 单例工厂

--- rpc-common/src/main/java/com/xizi/rpc/factory/SingletonFactory.java
+++ rpc-common/src/main/java/com/xizi/rpc/factory/SingletonFactory.java
@@ -10,11 +10,11 @@ import java.util.Map;
 public class SingletonFactory {
 
     private static Map<Class, Object> objectMap = new HashMap<>();
-
+    private volatile static Object instance;
     private SingletonFactory() {}
 
     public static <T> T getInstance(Class<T> clazz) {
-        Object instance = objectMap.get(clazz);
+        instance=objectMap.get(clazz);
         synchronized (clazz) {
             if(instance == null) {
                 try {
